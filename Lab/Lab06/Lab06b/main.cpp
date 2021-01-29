601514e9d55f3d1329cebbd0853034df4469abfa
austinwklein <austinwklein@users.noreply.github.com>
2021-01-29 10:29:35 -0900

    Lab06: Initialized and files added.

---
+++ Lab/Lab06/Lab06b/main.cpp
@@ -0,0 +1,25 @@
+// Boilerplate
+/*
+ * Austin Klein
+ * CS 201
+ * 2/01/2021
+ * Lab 6b
+ * LAB TOPIC
+ */
+
+// Description:
+/* Description from Blackboard
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+
+
+    return 0;
+}
