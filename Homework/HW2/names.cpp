496501a5d600a301299d4c149bdebb2f9d66b56a
Austin Klein <atvklein@gmail.com>
2021-01-31 11:37:18 -0900

    HW 2a: Compiles without errors.

--- Homework/HW2/names.cpp
+++ Homework/HW2/names.cpp
@@ -24,6 +24,12 @@ using std::getline;
 using std::vector;
 
 int main() {
-
+    vector<string> names;
+    for (int i = 0; i < 10; i ++){
+        string name;
+        cout << "Please enter a name: ";
+        getline(cin, name);
+        names.push_back(name);
+    }
     return 0;
 }
