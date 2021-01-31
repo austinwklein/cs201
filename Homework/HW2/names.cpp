6c7bc05e458aca0adf865e6d2d43f99ef3677e08
Austin Klein <atvklein@gmail.com>
2021-01-31 11:22:40 -0900

    HW 2: Initialized

---
+++ Homework/HW2/names.cpp
@@ -0,0 +1,29 @@
+// Boilerplate
+/*
+ * Austin Klein
+ * CS 201
+ * 1/31/2021
+ * HW 2
+ * names.cpp
+ */
+
+// Design
+/* Write a C++ program that uses the std::vector class with a variety of loops.
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+using std::vector;
+
+int main() {
+
+    return 0;
+}
