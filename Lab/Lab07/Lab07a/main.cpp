1de5ef10f2a2740b416de29a1f219b9a6d540b85
austinwklein <austinwklein@users.noreply.github.com>
2021-02-03 10:21:56 -0900

    Lab 7: Initialized and completed

--- Lab/Lab07/Lab07a/main.cpp
+++ Lab/Lab07/Lab07a/main.cpp
@@ -11,6 +11,7 @@
 /* Description from Blackboard
  */
 
+#include "intio.hpp"
 #include <iostream>
 
 using std::cout;
@@ -19,7 +20,19 @@ using std::endl;
 
 int main() {
 
+    cout << "Input a number: " << endl;
+    int a = getInt();
+    cout << "Input another number: " << endl;
+    int b = getInt();
+    cout << "Input a third number: " << endl;
+    int c = getInt();
 
+    putInt(a, 10);
+    cout << endl;
+    putInt(b, 10);
+    cout << endl;
+    putInt(c, 10);
+    cout << endl;
 
     return 0;
 }
