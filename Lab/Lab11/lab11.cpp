5fa89968016a994101ad9f626ec85cd8075f1171
austinwklein <austinwklein@users.noreply.github.com>
2021-02-10 10:20:18 -0900

    Lab 11: Setup completed with description from blackboard.

---
+++ Lab/Lab11/lab11.cpp
@@ -0,0 +1,53 @@
+// Boilerplate
+/*
+ * lab11.cpp
+ * Austin Klein
+ * CS 201
+ * Feb/12/2021
+ * Lab 11
+ * Characters in a string
+ */
+
+// Description:
+/* Write a program that repeatedly inputs a word from the user and then
+ * prints the characters in the word that are lower-case letters, in
+ * the order in which they appear in the word.
+ *
+ * Here, a  word is any sequence of non-space characters.
+ * For example, “ 3rq%d$EzZ@[x” counts as a word.
+ * If the above word is entered by the user, then the program should
+ * output the following.
+ *
+ *      rqdzx
+ *
+ * Can you make use of auto in your program?
+ * Possibly not. The use of the  auto keyword that we covered was introduced
+ * into C++ in the 2011 Standard, while your compiler may be configured to use
+ * an older standard. We will cover this issue in a forthcoming assignment.
+ * For now, give  auto a try, and see if your compiler will accept it.
+ *
+ * Expand your program so that it prints the lower-case letters, as in part 1,
+ * followed by the upper-case letters, on a separate line, followed by all
+ * other characters, on a third line.
+ *
+ * If the word from part 1 is entered by the user, then the program should
+ * output the following.
+ *
+ *      rqdzx
+ *      EZ
+ *      3%$@[
+ */
+
+#include <iostream>
+#include "lab11.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ Lab/Lab11/lab11.hpp
@@ -0,0 +1,24 @@
+// Boilerplate
+/*
+ * lab11.hpp
+ * Austin Klein
+ * CS 201
+ * Feb/12/2021
+ * Lab 11
+ * Characters in a string
+ */
+#ifndef LAB11_HPP
+#define LAB11_HPP
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+
+void function(){
+
+}
+
+#endif //LAB11_HPP
