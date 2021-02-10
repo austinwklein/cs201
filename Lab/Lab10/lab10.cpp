c97626247bcc3a2a0c48c049230a46a9203b85e9
austinwklein <austinwklein@users.noreply.github.com>
2021-02-09 22:03:23 -0900

    Lab10: Start

---
+++ Lab/Lab10/lab10.cpp
@@ -0,0 +1,25 @@
+// Boilerplate
+/*
+ * Austin Klein
+ * CS 201
+ * MONTH/DAY/2021
+ * Lab LAB NUMBER
+ * LAB TOPIC
+ */
+
+// Description:
+/* Description from Blackboard
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ Lab/Lab10/lab10.hpp
@@ -0,0 +1,14 @@
+//Boilerplate
+/*
+ * lab10.hpp
+ * Austin Klein
+ * CS 201
+ * Feb/10/2021
+ * Lab 10
+ * Boolean values
+ */
+
+#ifndef LAB_HEADER_HPP
+#define LAB_HEADER_HPP
+
+#endif //LAB_HEADER_HPP
