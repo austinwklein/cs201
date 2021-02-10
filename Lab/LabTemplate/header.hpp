592485dfc7cae524bd69afa2d13226384b3a9fe9
austinwklein <austinwklein@users.noreply.github.com>
2021-02-09 21:28:37 -0900

    Header template saved

---
+++ Lab/LabTemplate/header.hpp
@@ -0,0 +1,13 @@
+// Boilerplate
+/*
+ * file name
+ * Austin Klein
+ * CS 201
+ * MONTH/DAY/2021
+ * Lab LAB NUMBER
+ * LAB TOPIC
+ */
+#ifndef LAB_HEADER_HPP
+#define LAB_HEADER_HPP
+
+#endif //LAB_HEADER_HPP
