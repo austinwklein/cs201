34da8b7f687b336696c99fc286299810fbb71976
austinwklein <austinwklein@users.noreply.github.com>
2021-02-11 16:38:53 -0900

    HW 3b: collatz.cpp functions blueprinted and commented out until later. compiles.
    Need to: fix program run loop.

--- Homework/HW3/HW3b/collatz.cpp
+++ Homework/HW3/HW3b/collatz.cpp
@@ -1,4 +1,4 @@
-// Boilerplate
+// Boilerplate ******************************************************************
 /*
  * collatz.cpp
  * Austin Klein
@@ -8,8 +8,9 @@
  * Collatz
  */
 
-// Description:
-/* This program prints out the Collatz sequence for any number a user inputs.
+// Description:******************************************************************
+/*
+ * This program prints out the Collatz sequence for any number a user inputs.
  * If the user enters a negative number it will be filtered out and they will be
  * re-prompted.
  * Continues to re-prompt until user chooses to quit.
@@ -22,17 +23,52 @@
  *              { 3n + 1    if  n is odd
  *      c(n) =  { n/2       if  n is even
  *
+ * ******************************************************************************
  */
 
+
+// ******************************************************************************
+// **********       Preprocessing         ***************************************
+// ******************************************************************************
+
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
+
+
+
+// ******************************************************************************
+// **********       Functions             ***************************************
+// ******************************************************************************
+
+//void getNumber();
+
+//void processSequence();
+
+//void printSequence();
+
+
+
+// ******************************************************************************
+// **********       Main                  ***************************************
+// ******************************************************************************
 
 int main() {
 
-    cout << "Hello" << endl;
+    // Variables
+    int firstNumber = 0;
+    string sequenceOut;
+    bool runAgain = true;
+
+    while (runAgain) {
+        cout << "Press '1' to enter another number or press any other key and "
+                "ENTER to QUIT: ";
+        cin >> runAgain;
+    }
 
     return 0;
 }
