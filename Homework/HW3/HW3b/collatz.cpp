3d732ef9aeb79f9745c327c1ba5fc6d27da58e31
austinwklein <austinwklein@users.noreply.github.com>
2021-02-11 16:18:27 -0900

    HW 3b: collatz.cpp set up and initialized. Description added, compiles, NDN

---
+++ Homework/HW3/HW3b/collatz.cpp
@@ -0,0 +1,38 @@
+// Boilerplate
+/*
+ * collatz.cpp
+ * Austin Klein
+ * CS 201
+ * Feb/11/2021
+ * HW 3:
+ * Collatz
+ */
+
+// Description:
+/* This program prints out the Collatz sequence for any number a user inputs.
+ * If the user enters a negative number it will be filtered out and they will be
+ * re-prompted.
+ * Continues to re-prompt until user chooses to quit.
+ * Sequence is output to one line with a space between each number and a \n
+ * at the end of each line.
+ *
+ * Collatz sequence:
+ *          Piecewise:
+ *
+ *              { 3n + 1    if  n is odd
+ *      c(n) =  { n/2       if  n is even
+ *
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+    cout << "Hello" << endl;
+
+    return 0;
+}
