8b657d2e080499dfb25ef8140ee5c090bc78549f
austinwklein <austinwklein@users.noreply.github.com>
2021-02-15 22:31:18 -0900

    Reusable code

---
+++ Lab/LabTemplate/FunctionStash.cpp
@@ -0,0 +1,9 @@
+// So this is where all the reusable functions are hiding?
+
+#include "FunctionStash.h"
+
+void printVector(vector<int> &vectorName) {
+    for (int i = 0; i < vectorName.size(), i++) {
+        cout << vectorName[i] << endl;
+    }
+}
---
+++ Lab/LabTemplate/FunctionStash.h
@@ -0,0 +1,18 @@
+// Header for the secret function stash-- Don't tell anyone!
+
+#ifndef HW_FUNCTIONSTASH_H
+#define HW_FUNCTIONSTASH_H
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::ws;
+using std::vector;
+
+void printVector(vector<int> &vectorName);
+
+#endif //HW_FUNCTIONSTASH_H
