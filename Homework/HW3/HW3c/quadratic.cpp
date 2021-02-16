09858e13f767559850fa50f794665f81357bcbcf
austinwklein <austinwklein@users.noreply.github.com>
2021-02-15 23:52:36 -0900

    HW 3c: quadratic.cpp fully functional. Bugs: '0' prints as '-0' if it is multiplied by '-1' before being output.

--- Homework/HW3/HW3c/quadratic.cpp
+++ Homework/HW3/HW3c/quadratic.cpp
@@ -33,6 +33,7 @@ using std::cin;
 using std::endl;
 using std::ws;
 using std::string;
+using std::sqrt;
 
 
 
@@ -42,9 +43,31 @@ using std::string;
 
 // Takes in variables used in computations
 void inputVariables(float &a,float &b,float &c){
-    cout << endl;
     cout << "Input a, b, and c separated by spaces and press ENTER: ";
     cin >> a >> b >> c;
+}
+
+// Logic function of program
+void processRoots(float &a,float &b,float &c, float &xPlus, float &xMinus){
+    xPlus = (-1 * b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+    xMinus = (-1 * b - sqrt((b * b) - (4 * a * c))) / (2 * a);
+}
+
+void printResults(float &xPlus, float &xMinus) {
+    // Get special case of 1 root out of the way first
+    if (xPlus == xMinus) {
+        if (std::to_string(xMinus) != "-nan(ind)") cout << "X = " << -1 * xMinus << endl;
+    }
+    else {
+        // Check each root individually to see if they had a solution, if so, output
+        if (std::to_string(xPlus) != "-nan(ind)") cout << "X = " << -1 * xPlus << endl;
+        if (std::to_string(xMinus) != "-nan(ind)") cout << "X = " << -1 * xMinus << endl;
+
+        // If both the + and - are not real numbers or cannot be found, output message
+        if (std::to_string(xPlus) == "-nan(ind)" && std::to_string(xMinus) == "-nan(ind)") {
+            cout << "There are no real roots for this function. " << endl;
+        }
+    }
 }
 
 // When prompted, if user enters '1', the program will re-run
@@ -84,20 +107,31 @@ int main() {
     float a = 0;
     float b = 0;
     float c = 0;
+    // Roots
+    float xPlus = 0;
+    float xMinus = 0;
 
-    // Welcome
+    // Welcome: placed outside the loop as it will only be run one time
     cout << endl;
     cout << "Welcome to the Quadra-Solver 4000: the best quadratic formula calculator "
             "around! " << endl;
 
     // Program run loop
     while (runAgain) {
+        // Formatting and resetting roots to 0
+        xPlus = 0;
+        xMinus = 0;
+        cout << endl;
+
         // Variables for quadratic function
         inputVariables(a, b, c);
-        // processRoots(a, b, c);
-        // printResults(a, b, c);
-        // loopProgram(loop);
-        // Program run loop
+        cout << endl;
+
+        processRoots(a, b, c, xPlus, xMinus);
+        cout << endl;
+
+        printResults(xPlus, xMinus);
+        cout << endl;
 
         // Prompts user to enter another number or exit
         runAgain = RunAgain();
