bbaecb328bd2b0757b0a81e3d473e3f8ba61755e
austinwklein <austinwklein@users.noreply.github.com>
2021-02-17 10:31:18 -0900

    Template: main.cpp updated

--- Lab/LabTemplate/main.cpp
+++ Lab/LabTemplate/main.cpp
@@ -1,25 +1,84 @@
-// Boilerplate
-/*
- * Austin Klein
+// Boilerplate ******************************************************************
+/* Austin Klein
  * CS 201
- * MONTH/DAY/2021
- * Lab LAB NUMBER
- * LAB TOPIC
+ * Feb/[DAY]/2021
+ * Lab [LAB NUMBER]
+ * [ASSIGNMENT FILE NAME]
+ * */
+
+// Description:******************************************************************
+/*
+ * [DESCRIPTION FROM BLACKBOARD]
+ * ******************************************************************************
  */
 
-// Description:
-/* Description from Blackboard
- */
+
+
+// ******************************************************************************
+// **********       Preprocessing         ***************************************
+// ******************************************************************************
 
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ws;
+using std::string;
+using std::sqrt;
+
+
+
+// ******************************************************************************
+// **********       Functions             ***************************************
+// ******************************************************************************
+
+// When prompted, if user enters '1', the program will re-run
+// any other output ends the program
+bool RunAgain(){
+    /* loop = temporary variable for user input to loop the program for additional
+     * entry
+     */
+    string loop;
+
+    cout << "Press '1' to [WHATEVER PROGRAM DOES] or press any other key and "
+            "ENTER to QUIT: ";
+
+    // getline used to erase anything left in the stream from the last time through
+    // the loop and then take new input from the user
+    getline(cin >> ws, loop);
+    if (loop == "1") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+// ******************************************************************************
+// **********       Main                  ***************************************
+// ******************************************************************************
 
 int main() {
 
+    /*\**********************
+    ***    Variables     ****
+    **********************\*/
+    bool runAgain = true;
 
+
+    // Message outside the loop that executes once
+    cout << endl;
+    cout << "Welcome! " << endl;
+
+    // Program run loop
+    while (runAgain) {
+
+        // Prompts user to enter another number or exit
+        runAgain = RunAgain();
+    }
 
     return 0;
 }
